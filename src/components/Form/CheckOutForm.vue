844d7f8ccec674d40cd6b18da94492948af21754
khanh <khanhnqps10920@fpt.edu.vn>
2020-08-25 15:16:26 +0700

    update checkout page

---
+++ src/components/Form/CheckOutForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-form class="checkout-form">
+    <b-form-group label="Contact Information" label-for="email">
+      <b-form-input type="email" required placeholder="Your Email"></b-form-input>
+    </b-form-group>
+    <b-form-group>
+      <b-form-checkbox value="keep">Keep me up to date on news and exclusive offers</b-form-checkbox>
+    </b-form-group>
+
+    <label class="mt-5 mb-3">Shipping Address</label>
+    <div class="d-flex checkout-form__inps">
+      <b-form-group>
+        <b-form-input type="text" required placeholder="First Name"></b-form-input>
+      </b-form-group>
+      <b-form-group>
+        <b-form-input type="text" required placeholder="Last Name"></b-form-input>
+      </b-form-group>
+    </div>
+    <b-form-group>
+      <b-form-input type="text" placeholder="Company(optional)"></b-form-input>
+    </b-form-group>
+    <b-form-group>
+      <b-form-input type="text" required placeholder="Address"></b-form-input>
+    </b-form-group>
+    <b-form-group>
+      <b-form-input type="text" placeholder="Apartment, suite, etc.(optional)"></b-form-input>
+    </b-form-group>
+    <b-form-group>
+      <b-form-input type="text" required placeholder="City"></b-form-input>
+    </b-form-group>
+  </b-form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/variable.scss";
+</style>
--- src/views/CheckOut.vue
+++ src/views/CheckOut.vue
@@ -3,7 +3,21 @@
     <b-container fluid>
       <b-row>
         <b-col sm="7">
-          <div class="checkout__information"></div>
+          <div class="checkout__information">
+            <div class="checkout__information--logo">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0027/5382/2835/files/Screen_Shot_2018-07-27_at_10.13.38_AM.png?4735"
+                class="img-fluid"
+                alt
+              />
+            </div>
+
+            <div class="checkout__information--line">
+              <span>Your Information</span>
+            </div>
+
+            <CheckOutForm />
+          </div>
         </b-col>
         <b-col sm="5">
           <div class="checkout__total">
@@ -16,20 +30,53 @@
 </template>
 
 <script>
-export default {};
+import CheckOutForm from "../components/Form/CheckOutForm.vue";
+// components
+
+export default {
+  components: {
+    CheckOutForm
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/scss/variable.scss";
 
 .checkout {
+  // infor
   &__information {
     border-right: 0.1rem solid #e6e6e6;
-    height: 700px;
 
-    padding: 8rem 0;
+    padding: 8rem 8rem;
+
+    &--logo {
+      width: 10rem;
+    }
+
+    &--line {
+      margin-top: 4rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      text-align: center;
+
+      &::before,
+      &::after {
+        content: "";
+        height: 0.1rem;
+        background: #e6e6e6;
+        display: inline-block;
+        width: 40%;
+      }
+
+      span {
+        display: inline-block;
+      }
+    }
   }
 
+  // total
   &__total {
     position: fixed;
     padding: 8rem 0;
