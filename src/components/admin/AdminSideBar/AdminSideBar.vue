7557431bb0a0a7f526282d78f3f83f26c6e32771
unknown <khanhnqps10920@fpt.edu.vn>
2020-08-29 16:58:05 +0700

    add sidebar

---
+++ src/components/admin/AdminSideBar/AdminSideBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="ad-sidebar">
+    <ul class="ad-sidebar__menu">
+      <li class="ad-sidebar__menu-item">
+        <router-link to="/">
+          <b-icon-clipboard-data></b-icon-clipboard-data>Dashboard
+        </router-link>
+      </li>
+      <li class="ad-sidebar__menu-item">
+        <router-link to="/">
+          <b-icon-clipboard-data></b-icon-clipboard-data>Dashboard
+        </router-link>
+      </li>
+      <li class="ad-sidebar__menu-item">
+        <router-link to="/">
+          <b-icon-clipboard-data></b-icon-clipboard-data>Dashboard
+        </router-link>
+      </li>
+      <li class="ad-sidebar__menu-item">
+        <router-link to="/">
+          <b-icon-clipboard-data></b-icon-clipboard-data>Dashboard
+        </router-link>
+      </li>
+      <li class="ad-sidebar__menu-item">
+        <router-link to="/">
+          <b-icon-clipboard-data></b-icon-clipboard-data>Dashboard
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variable.scss";
+
+.ad-sidebar {
+  height: 100%;
+  overflow: hidden;
+
+  &__menu {
+    width: 100%;
+
+    li {
+      padding: 2rem;
+      transition: 0.4s ease;
+
+      a {
+        color: rgb(191, 203, 217);
+        svg {
+          margin-right: 1rem;
+        }
+      }
+
+      &:hover {
+        background: $second-color;
+        cursor: pointer;
+        a {
+          text-decoration: none;
+          color: $white-color;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -1,10 +1,58 @@
 <template>
-  <h1>Admin</h1>
+  <div class="admin admin-wrapper">
+    <div class="admin__sidebar-container">
+      <AdminSideBar />
+    </div>
+
+    <div class="admin__main-container">
+      <h1>test</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+// components
+import AdminSideBar from "../../components/admin/AdminSideBar/AdminSideBar.vue";
+
+export default {
+  components: {
+    AdminSideBar
+  }
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "@/assets/scss/variable.scss";
+
+.admin {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  a,
+  p,
+  span {
+    font-family: "Quicksand", sans-serif;
+  }
+
+  .admin-wrapper {
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+
+  &__sidebar-container {
+    width: 21rem !important;
+    background: $base-color;
+    height: 100%;
+    position: fixed;
+    overflow: hidden;
+    z-index: 9999;
+    top: 0;
+    left: 0;
+    bottom: 0;
+  }
+}
 </style>
